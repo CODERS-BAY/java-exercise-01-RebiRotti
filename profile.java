6b0734b94d581abb3a7c71ef9a137b3eb556dcfa
Rebecca <office@rebecca-rottensteiner.at>
2020-06-19 16:50:29 +0200

    First Java Exercise

---
+++ profile.java
@@ -0,0 +1,21 @@
+
+public class profile {
+	
+	public static void main (String[] args) {
+		String firstName = "Rebecca";
+		String familyName = "Rottensteiner";
+		int age = 36;
+		boolean married = false;
+		double salary = 1624.84;
+		char euro = 'E';
+		String address = "Heindlstr. 23 \r\n4040 Linz";
+		System.out.println("Name: " + firstName + " " + familyName);
+		System.out.println("--------------------------------------");
+		System.out.println("Alter: " + age + "\r\n" + "Verheiratet: " + married + "\r\n" + "Verdienst: " + euro + " " + salary);
+		System.out.println("--------------------------------------");
+		System.out.println(address);
+		
+		
+		
+	}
+}
